686957649545f5c75ff6588dae210fcf9391eb29
alexandreleao <erdleosalgueiro@gmail.com>
2021-10-20 17:17:55 -0300

    Iniciando novo projeto

---
+++ app/Model/Usuario.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Usuario extends Model
+{
+    protected $connection = 'sqlite';
+    protected $table =  'usuario';
+
+    public static function listar(int $limite){
+        $sql = self::select([
+            "id",
+            "nome",
+            "email",
+            "data_cadastro"
+        ])
+        ->limit($limite)
+        ->get();
+    }
+}
